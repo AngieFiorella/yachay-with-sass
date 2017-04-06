955d7a61f51e5b1bf95216fb7695388cb27387b5
Angie Barrantes <angiefiorellacat@gmail.com>
2017-04-06 16:43:48 -0500

    Updating styles with shadows

--- js/app.js
+++ js/app.js
@@ -2,10 +2,23 @@ $(document).ready(function(){
   carousel();
 });
 
-$('#js-menu-mobile').on('click', menuMobile);
+var topNav = $('#js-sticky-nav').offset().top;
+
+$('#js-menu-mobile').click(menuMobile);
+$(window).scroll(stickyNav);
 
 function menuMobile() {
   $(this).parent().toggleClass('active');
+}
+
+function stickyNav() {
+  var scrollWindow = $(window).scrollTop();
+
+  if (scrollWindow >= topNav) {
+    $('#js-sticky-nav').addClass('sticky');
+  } else if (scrollWindow < topNav) {
+    $('#js-sticky-nav').removeClass('sticky');
+  }
 }
 
 function carousel() {
@@ -13,7 +26,18 @@ function carousel() {
     loop: true,
     margin: 20,
     nav: false,
-    items:4,
     autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+          items:1
+        },
+        480:{
+          items:2
+        },
+        768:{
+          items:4
+        }
+    }
   });
 }
