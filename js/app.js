733f1fbe9b6d2acbc8e0ba0a6c9f3a5448ff45be
Angie Barrantes <angiefiorellacat@gmail.com>
2017-04-07 10:47:01 -0500

    updating responsive design for mobiles

--- js/app.js
+++ js/app.js
@@ -22,22 +22,40 @@ function stickyNav() {
 }
 
 function carousel() {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 20,
-    nav: false,
-    autoplay: true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-          items:1
-        },
-        480:{
-          items:2
-        },
-        768:{
-          items:4
-        }
-    }
-  });
+
+  var slider = $('.js-carousel-content'),
+      item = slider.find('.item'),
+      btnPrev = slider.parents('.slider').find('.js-controller.left'),
+      btnNext = slider.parents('.slider').find('.js-controller.right');
+
+  if (slider.length > 0 ) {
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: false,
+      autoplay: true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+            items:1
+          },
+          480:{
+            items:2
+          },
+          768:{
+            items:4
+          }
+      }
+    });
+  }
+
+  if (slider.length > 0 && item.length > 0 ) {
+    btnPrev.click(function() {
+      slider.trigger('prev.owl');
+    });
+    btnNext.click(function() {
+      slider.trigger('next.owl');
+    });
+  }
+
 }
